f23e4100e4c860dc2e7ddfb335ba22e08301d04b
Lineked <lineked@yandex.ru>
2024-03-07 13:33:11 +0300

    Добавлен ProductsRemoteDataSource

--- app/src/main/java/com/example/vktestproducts/data/remote/ProductsRemoteDataSource.kt
+++ app/src/main/java/com/example/vktestproducts/data/remote/ProductsRemoteDataSource.kt
@@ -1,4 +1,7 @@
 package com.example.vktestproducts.data.remote
 
+import com.example.vktestproducts.data.models.ResponseProducts
+
 interface ProductsRemoteDataSource {
+    suspend fun getProducts(skip: Int, limit: Int): ResponseProducts
 }
--- app/src/main/java/com/example/vktestproducts/data/remote/ProductsRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/vktestproducts/data/remote/ProductsRemoteDataSourceImpl.kt
@@ -1,4 +1,8 @@
 package com.example.vktestproducts.data.remote
 
-class ProductsRemoteDataSourceImpl: ProductsRemoteDataSource {
+import com.example.vktestproducts.data.models.ResponseProducts
+import javax.inject.Inject
+
+class ProductsRemoteDataSourceImpl @Inject constructor(private val productsService: ProductsService): ProductsRemoteDataSource {
+    override suspend fun getProducts(skip: Int, limit: Int): ResponseProducts = productsService.getProducts(skip, limit)
 }
