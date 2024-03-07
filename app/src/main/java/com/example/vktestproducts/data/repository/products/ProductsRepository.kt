21671ea04acf8a107be5a3fffd9b7a93f384a1c5
Lineked <lineked@yandex.ru>
2024-03-07 23:43:05 +0300

    Добавлена функция productsPagingSource

--- app/src/main/java/com/example/vktestproducts/data/repository/products/ProductsRepository.kt
+++ app/src/main/java/com/example/vktestproducts/data/repository/products/ProductsRepository.kt
@@ -1,4 +1,7 @@
 package com.example.vktestproducts.data.repository.products
 
+import com.example.vktestproducts.data.remote.ProductsPagingSource
+
 interface ProductsRepository {
+    fun productsPagingSource(): ProductsPagingSource
 }
--- app/src/main/java/com/example/vktestproducts/data/repository/products/ProductsRepositoryImpl.kt
+++ app/src/main/java/com/example/vktestproducts/data/repository/products/ProductsRepositoryImpl.kt
@@ -1,4 +1,9 @@
 package com.example.vktestproducts.data.repository.products
 
-class ProductsRepositoryImpl: ProductsRepository {
+import com.example.vktestproducts.data.remote.ProductsPagingSource
+import com.example.vktestproducts.data.remote.ProductsRemoteDataSource
+import javax.inject.Inject
+
+class ProductsRepositoryImpl @Inject constructor(private val productsRemoteDataSource: ProductsRemoteDataSource): ProductsRepository {
+    override fun productsPagingSource(): ProductsPagingSource = ProductsPagingSource(productsRemoteDataSource)
 }
