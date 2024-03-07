ed83e9d403156e2be4fcba90fd7fca747825ba94
Lineked <lineked@yandex.ru>
2024-03-07 13:34:04 +0300

    Hilt

--- app/src/main/java/com/example/vktestproducts/VkTestProductsApplication.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vktestproducts
-
-import android.app.Application
-
-class VkTestProductsApplication: Application() {
-}
---
+++ app/src/main/java/com/example/vktestproducts/di/VkTestProductsApplication.kt
@@ -0,0 +1,8 @@
+package com.example.vktestproducts.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class VkTestProductsApplication: Application() {
+}
