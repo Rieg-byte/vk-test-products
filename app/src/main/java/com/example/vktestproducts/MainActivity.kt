b39f746849931891e70f6a9b765251c206b82e5a
Lineked <lineked@yandex.ru>
2024-03-07 23:39:58 +0300

    Небольшие изменения

--- app/src/main/java/com/example/vktestproducts/MainActivity.kt
+++ app/src/main/java/com/example/vktestproducts/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.vktestproducts.presentation.products.ProductsScreen
 import com.example.vktestproducts.ui.theme.VkTestProductsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,7 +18,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             VkTestProductsTheme {
                 // A surface container using the 'background' color from the theme
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ProductsScreen()
+                }
             }
         }
     }
