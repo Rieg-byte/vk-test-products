cfd176f9cea2b9191840898405d6d0131efccdb7
Lineked <lineked@yandex.ru>
2024-03-07 23:42:32 +0300

    Добавлен модуль ProductsModule с предоставлением зависимостей

--- app/src/main/java/com/example/vktestproducts/di/modules/ProductsModule.kt
+++ app/src/main/java/com/example/vktestproducts/di/modules/ProductsModule.kt
@@ -1,4 +1,24 @@
 package com.example.vktestproducts.di.modules
 
+import com.example.vktestproducts.data.remote.ProductsRemoteDataSource
+import com.example.vktestproducts.data.remote.ProductsRemoteDataSourceImpl
+import com.example.vktestproducts.data.repository.products.ProductsRepository
+import com.example.vktestproducts.data.repository.products.ProductsRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 abstract class ProductsModule {
+    @Binds
+    abstract fun provideProductsRemoteDataSource(
+        productsRemoteDataSource: ProductsRemoteDataSourceImpl
+    ): ProductsRemoteDataSource
+
+    @Binds
+    abstract fun provideProductsRepository(
+        productsRepository: ProductsRepositoryImpl
+    ): ProductsRepository
 }
