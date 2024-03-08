d2141486705684f9639457fdf68670c3ad7fbc66
Lineked <lineked@yandex.ru>
2024-03-08 21:07:07 +0300

    Добавлен ProductsViewModel

--- app/src/main/java/com/example/vktestproducts/presentation/products/ProductsViewModel.kt
+++ app/src/main/java/com/example/vktestproducts/presentation/products/ProductsViewModel.kt
@@ -1,6 +1,22 @@
 package com.example.vktestproducts.presentation.products
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.vktestproducts.data.models.Product
+import com.example.vktestproducts.data.repository.products.ProductsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class ProductsViewModel: ViewModel() {
+@HiltViewModel
+class ProductsViewModel @Inject constructor(private val productsRepository: ProductsRepository): ViewModel() {
+    private val _productsState: MutableStateFlow<ProductsState> = MutableStateFlow(ProductsState())
+    val productsState: StateFlow<ProductsState> = _productsState.asStateFlow()
+    val products: Flow<PagingData<Product>> = productsRepository.getProducts().cachedIn(viewModelScope)
+
 }
