08b90ee05512032a8004ac91bb8a68a82bc273bb
Lineked <lineked@yandex.ru>
2024-03-08 21:03:02 +0300

    Добавлен заполнитель для отображения загрузки

---
+++ app/src/main/java/com/example/vktestproducts/presentation/components/LoadingPlaceholder.kt
@@ -0,0 +1,29 @@
+package com.example.vktestproducts.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoadingPlaceholder(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Preview(showBackground = true, device = "id:pixel_4")
+@Composable
+private fun LoadingPlaceholderPreview() {
+    LoadingPlaceholder()
+}
