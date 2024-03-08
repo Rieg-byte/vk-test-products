4aca351b78a68cbe94d2e6ae4f84c02d099b88bd
Lineked <lineked@yandex.ru>
2024-03-08 23:31:59 +0300

    Добавленые новые зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     implementation(libs.retrofit)
 
