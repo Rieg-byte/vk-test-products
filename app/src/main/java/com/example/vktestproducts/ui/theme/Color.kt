bc5c7a986ae5419471eeae3b81b9c617404afa6a
Lineked <lineked@yandex.ru>
2024-03-09 20:38:37 +0300

    Добавдены два дополнительных цвета

--- app/src/main/java/com/example/vktestproducts/ui/theme/Color.kt
+++ app/src/main/java/com/example/vktestproducts/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val discountBackgroundColor = Color(0xFF42a500)
+val onDiscountBackgroundColor = Color(0xFFFFFFFF)
+val colorRating = Color(0xffffa800)
--- app/src/main/java/com/example/vktestproducts/ui/theme/Theme.kt
+++ app/src/main/java/com/example/vktestproducts/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.example.vktestproducts.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -10,6 +11,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -24,18 +26,21 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
+    tertiary = Pink40,
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onSurface = Color(0xFF1C1B1F)
 )
+
+val ColorScheme.discountBackground: Color @Composable
+    get() = discountBackgroundColor
+
+val ColorScheme.onDiscountBackground: Color @Composable
+    get() = onDiscountBackgroundColor
 
 @Composable
 fun VkTestProductsTheme(
