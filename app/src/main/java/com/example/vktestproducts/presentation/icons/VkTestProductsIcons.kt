fa516c9d46972b7ee23ba4aa570734d266585289
Lineked <lineked@yandex.ru>
2024-03-09 20:42:16 +0300

    Добавлен VkTestProductsIcons

---
+++ app/src/main/java/com/example/vktestproducts/presentation/icons/VkTestProductsIcons.kt
@@ -0,0 +1,8 @@
+package com.example.vktestproducts.presentation.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+
+object VkTestProductsIcons {
+    val ArrowBack = Icons.AutoMirrored.Rounded.ArrowBack
+}
