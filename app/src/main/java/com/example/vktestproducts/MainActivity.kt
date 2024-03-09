21350a86271ba2e4406fd2c2f0a3701924fce700
Lineked <lineked@yandex.ru>
2024-03-09 20:41:16 +0300

    ProductsScreen заменен на VkTestProductsNavHost

--- app/src/main/java/com/example/vktestproducts/MainActivity.kt
+++ app/src/main/java/com/example/vktestproducts/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.vktestproducts.presentation.navigation.VkTestProductsNavHost
 import com.example.vktestproducts.presentation.products.ProductsScreen
 import com.example.vktestproducts.ui.theme.VkTestProductsTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ProductsScreen()
+                    VkTestProductsNavHost()
                 }
             }
         }
