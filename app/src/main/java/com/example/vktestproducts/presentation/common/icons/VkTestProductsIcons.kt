b712427ffdbf3d39d922fe53a531e65cf3113501
Lineked <lineked@yandex.ru>
2024-03-10 20:53:08 +0300

    Добавлены две иконки

--- app/src/main/java/com/example/vktestproducts/presentation/common/icons/VkTestProductsIcons.kt
+++ app/src/main/java/com/example/vktestproducts/presentation/common/icons/VkTestProductsIcons.kt
@@ -2,7 +2,11 @@ package com.example.vktestproducts.presentation.common.icons
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Search
 
 object VkTestProductsIcons {
     val ArrowBack = Icons.AutoMirrored.Rounded.ArrowBack
+    val Search = Icons.Rounded.Search
+    val Close = Icons.Rounded.Close
 }
