37e3864555b4eb0a283a65e59e21e5f96ac6223f
Lineked <lineked@yandex.ru>
2024-03-11 20:18:10 +0300

    Небольшие изменения

--- app/src/main/java/com/example/vktestproducts/presentation/common/components/LoadingPlaceholder.kt
+++ app/src/main/java/com/example/vktestproducts/presentation/common/components/LoadingPlaceholder.kt
@@ -1,11 +1,15 @@
 package com.example.vktestproducts.presentation.common.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.vktestproducts.ui.theme.VkTestProductsTheme
 
 @Composable
 fun LoadingPlaceholder(
@@ -19,8 +23,13 @@ fun LoadingPlaceholder(
     }
 }
 
-@Preview(showBackground = true, device = "id:pixel_4")
+@Preview(name = "Dark theme", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "Light theme", showBackground = true)
 @Composable
 private fun LoadingPlaceholderPreview() {
-    LoadingPlaceholder()
+    VkTestProductsTheme {
+        Surface {
+            LoadingPlaceholder(modifier = Modifier.fillMaxSize())
+        }
+    }
 }
