3ebdc5691be007af0b8fee80b7e9eba9a0a35fdf
Lineked <lineked@yandex.ru>
2024-03-12 18:44:55 +0300

    Добавлены новые обработчики пользовательского события

--- app/src/main/java/com/example/vktestproducts/presentation/products/ProductsViewModel.kt
+++ app/src/main/java/com/example/vktestproducts/presentation/products/ProductsViewModel.kt
@@ -9,6 +9,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,9 +23,80 @@ class ProductsViewModel @Inject constructor(private val productsRepository: Prod
         getProducts()
     }
 
+    fun showCategoriesBottomSheet() {
+        _productsState.update {
+            it.copy(showBottomSheet = true)
+        }
+        getCategories()
+    }
+
+    fun closeCategoriesBottomSheet() = _productsState.update {
+        it.copy(
+            showBottomSheet = false
+        )
+    }
+
+    fun onSelectCategory(nameCategory: String) {
+        _productsState.update {
+            it.copy(
+                categoryIsSelect = true,
+                nameSelectedCategory = nameCategory
+            )
+        }
+        getProductsByCategory(nameCategory)
+    }
+
+    fun onDeselectCategory() {
+        _productsState.update {
+            it.copy(
+                categoryIsSelect = false,
+                nameSelectedCategory = ""
+            )
+        }
+        getProducts()
+    }
+
+    fun onRefreshCategories() {
+        _productsState.update {
+            it.copy(
+                categoriesState = CategoriesState.Loading
+            )
+        }
+        getCategories()
+    }
+
+    private fun getCategories() = viewModelScope.launch {
+        try {
+            val categories = productsRepository.getCategories()
+            _productsState.update {
+                it.copy(
+                    categoriesState = CategoriesState.Success(categories = categories)
+                )
+            }
+        } catch (e: HttpException) {
+            _productsState.update {
+                it.copy(
+                    categoriesState = CategoriesState.Error
+                )
+            }
+        } catch (e: IOException) {
+            _productsState.update {
+                it.copy(
+                    categoriesState = CategoriesState.Error
+                )
+            }
+        }
+    }
+
     private fun getProducts() = _productsState.update {
         it.copy(
             products = productsRepository.getProducts().cachedIn(viewModelScope)
+        )
+    }
+
+    private fun getProductsByCategory(nameCategory: String) = _productsState.update {
+        it.copy(
+            products = productsRepository.getProductsByCategory(nameCategory).cachedIn(viewModelScope)
         )
     }
 }
