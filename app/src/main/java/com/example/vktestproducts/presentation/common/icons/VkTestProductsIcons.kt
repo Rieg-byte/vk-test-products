4403f64014cc1fa09956f0d4c966dd5ff43c2193
Lineked <lineked@yandex.ru>
2024-03-12 19:48:33 +0300

    Добавлена иконка Cancel

--- app/src/main/java/com/example/vktestproducts/presentation/common/icons/VkTestProductsIcons.kt
+++ app/src/main/java/com/example/vktestproducts/presentation/common/icons/VkTestProductsIcons.kt
@@ -2,6 +2,7 @@ package com.example.vktestproducts.presentation.common.icons
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Cancel
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.Search
 
@@ -9,4 +10,5 @@ object VkTestProductsIcons {
     val ArrowBack = Icons.AutoMirrored.Rounded.ArrowBack
     val Search = Icons.Rounded.Search
     val Close = Icons.Rounded.Close
+    val Cancel = Icons.Rounded.Cancel
 }
